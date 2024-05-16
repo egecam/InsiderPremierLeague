e071eeafca6003cccaa2e34412a44899bf7042fa
ege <egecam000@gmail.com>
2024-05-16 15:10:18 +0300

    Initial Commit

---
+++ InsiderPremierLeague/InsiderPremierLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsiderPremierLeagueApp.swift
+//  InsiderPremierLeague
+//
+//  Created by Ege Çam on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct InsiderPremierLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
